075fa27524af42fa2a14128e1df16f15bb2a1268
Karsten Jeppesen <karsten@jeppesens.com>
2021-04-14 18:39:43 +0200

    NioLineCount: Working

--- NioLineCount/src/NioLineCount.java
+++ NioLineCount/src/NioLineCount.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Semaphore;
 
 /**
  * 
@@ -16,31 +19,61 @@ import java.nio.file.StandardOpenOption;
  *
  */
 public class NioLineCount {
+	
+	static int theCount = 0;
+	// A 0 means that you will be waited until signalled
+	static Semaphore partialReadGate = new Semaphore(1);
+	static boolean doContinue = true;
+	static long readPosition = 0;
 
 	private static int lineCount(String thePath, boolean doDance){
 		if ( ! doDance ) return -1;
-		ByteBuffer buffer = java.nio.ByteBuffer.allocate(1024);
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		Path myPath = Paths.get( thePath );
 		try {
 			AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(myPath, StandardOpenOption.READ);
-			fileChannel.read(buffer, 0, buffer, new CompletionHandler<Integer,ByteBuffer>() {
-				@Override
-				public void completed(Integer result, ByteBuffer attachment) {
-					System.out.println("Result = " + result);
-					attachment.flip();
-					byte[] data = new byte[attachment.limit()];
-					attachment.clear();
+			while ( doContinue ) {
+				try {
+					// In the end we will be waiting here
+					partialReadGate.acquire();
+					if ( ! doContinue ) return theCount;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			
-				@Override
-				public void failed(Throwable exc, ByteBuffer attachment) {
-				}
-			});
+				fileChannel.read(buffer, readPosition, buffer, new CompletionHandler<Integer,ByteBuffer>() {
+					@Override
+					public void completed(Integer result, ByteBuffer attachment) {
+						// System.out.println("Result = " + result + " Limit = " + attachment.limit());
+						if ( result != -1 ) {
+						attachment.flip();
+						byte[] data = new byte[attachment.limit()];
+						attachment.get( data );
+						//System.out.println(new String(data));
+						for (int i = 0; i < data.length; i++ ) {
+							if ( data[i] == '\n' )
+								theCount++;
+							//System.out.println("data: " + data[i]);
+						}
+						// System.out.println("position: " + readPosition + " Count: " + theCount);
+						readPosition += data.length;
+						attachment.clear();
+						} else {
+							doContinue = false;
+						}
+						partialReadGate.release();
+					}
+				
+					@Override
+					public void failed(Throwable exc, ByteBuffer attachment) {
+					}
+				});
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return 0;
+		return theCount;
 	}
 	
 	/**
